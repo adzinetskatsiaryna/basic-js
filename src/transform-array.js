3b8f48451bf0782b90891e2b92160aebab1f0f1a
adzinets <adzinetskatsiaryna@gmail.com>
2021-09-20 12:00:45 +0300

    add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,80 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+  let result = []
+
+  arr.forEach((item, i,arr)=> {
+    if (item !== '--discard-next' && item !== '--discard-prev' && item !== '--double-next' && item !== '--double-prev') {
+      result.push(item);
+
+      if (arr[i - 1] === '--discard-next') {
+        result.pop()
+      }
+      if (arr[i - 1] === '--double-next') {
+        result.push(item)
+      }
+      if (arr[i + 1] === '--discard-prev' && arr[i - 1] !== '--discard-next') {
+        result.pop()
+      }
+      if (arr[i + 1] === '--double-prev' && arr[i - 1] !== '--discard-next') {
+        result.push(item)
+      }
+    }
+  });
+
+  return result;
 }
+
+// export default function transform( arr ) {
+//   let array = [...arr];
+//   if (!Array.isArray(arr)) {
+//     throw new Error("'arr' parameter must be an instance of the Array!");
+//   }
+//   if (arr.length === 0) {
+//     return [];
+//   }
+
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === '--double-next') {
+//       if (typeof array[i+1] === 'number') {
+//         array[i] = array[i+1];
+//       } else {
+//         delete array[i];
+//       }
+//     }
+//     if (array[i] === '--double-prev') {     
+//       if (typeof array[i-1] === 'number') {
+//         array[i] = array[i-1];
+//       } else {
+//         delete array[i];
+//       }
+//     }
+//     if (array[i] === '--discard-next') {
+//       if (typeof array[i+1] === 'number') {
+//         delete array[i];
+//         delete array[i+1];
+//       } else {
+//         delete array[i];
+//       }
+//     }
+//     if (array[i] === '--discard-prev') {
+//       if (typeof array[i-1] === 'number') {
+//         delete array[i-1];
+//         delete array[i];
+//       } else {
+//         delete array[i];
+//       }  
+//     }
+//   }
+
+//   array = array.filter(item => item !== '')
+
+//   return array;
+// }
