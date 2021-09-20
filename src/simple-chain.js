4a5fd9eb20aca32e2c9307f8889ee577d3cf2c0c
adzinets <adzinetskatsiaryna@gmail.com>
2021-09-20 14:06:11 +0300

    add solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,39 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  arr:[],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    if(value === ''){
+      this.arr.push( `( )` )
+    }
+    this.arr.push(`( ${value} )`) 
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+   if(typeof position !== 'number' || position < 1 || position > this.arr.length){
+     this.arr = []
+     throw new Error("You can't remove incorrect link!")
+   } 
+     this.arr.splice(position - 1, 1)
+     return this
+    
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.getLength() !==0){
+      this.arr.reverse()
+    }
+    return this
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   let str = this.arr.join('~~')
+    this.arr = []
+    return str
   }
 };
